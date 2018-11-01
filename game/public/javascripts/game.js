e82a57693264067b43ab0635deafc9c752280492
Braden Huffman <braden.huffman99@gmail.com>
2018-11-01 04:53:33 +0000

    recent

--- game/public/javascripts/game.js
+++ game/public/javascripts/game.js
@@ -5,15 +5,18 @@ function mainControl($scope, $https) {
 
 }
 
-var myGamePiece;
 window.onload = startGame;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Game
 
 var myGamePiece;
+var myScore;
+var myObstacle;
 
 function startGame() {
     myGamePiece = new component(30, 30, "red", 10, 120);
+    //myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myObstacle = new component(10, 100, "green", 300, 170);
     myGameArea.start();
 }
 
@@ -24,11 +27,20 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
     }
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
 }
 
 
@@ -52,6 +64,25 @@ function component(width, height, color, x, y) {
         this.y += this.speedY + this.gravitySpeed;
         this.hitBottom();
     }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
         if (this.y > rockbottom) {
@@ -59,20 +90,26 @@ function component(width, height, color, x, y) {
             this.gravitySpeed = 0;
             this.speedY = 0;
         }
-        
+
     }
 }
 
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
+    if (myGamePiece.crashWith(myObstacle)) {
+        myGameArea.stop();
+    }
+    else {
+        myGameArea.clear();
+        myObstacle.x += -2.5;
+        myGamePiece.newPos();
+        myGamePiece.update();
+        myObstacle.update();
+    }
 }
 
 function moveup() {
-    if (myGamePiece.gravitySpeed == 0)
-    {
-      myGamePiece.speedY -= 9;      
+    if (myGamePiece.gravitySpeed == 0) {
+        myGamePiece.speedY -= 9;
     }
 }
 

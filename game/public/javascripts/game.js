68093f7603657985b0ead41e492dfbef666b8f0b
Braden Huffman <braden.huffman99@gmail.com>
2018-11-01 17:58:50 +0000

    stick figure added

--- game/public/javascripts/game.js
+++ game/public/javascripts/game.js
@@ -8,13 +8,13 @@ function mainControl($scope, $https) {
 window.onload = startGame;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Game
-
+var onBottom;
 var myGamePiece;
 var myScore;
 var myObstacle;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece = new component(11, 21, "stickman.jpg", 10, 120, "image");
     //myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myObstacle = new component(10, 100, "green", 300, 170);
     myGameArea.start();
@@ -39,12 +39,17 @@ var myGameArea = {
 }
 
 function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
     return false;
 }
 
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -54,9 +59,17 @@ function component(width, height, color, x, y) {
     this.gravity = 0.3;
     this.gravitySpeed = 0;
     this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (type == "image") {
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width, this.height);
+        }
+        else {
+            ctx = myGameArea.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     this.newPos = function() {
         this.x += this.speedX;
@@ -89,35 +102,89 @@ function component(width, height, color, x, y) {
             this.y = rockbottom;
             this.gravitySpeed = 0;
             this.speedY = 0;
+            onBottom = true;
+        }
+        else {
+            onBottom = false;
         }
 
     }
 }
+var myObstacles = [];
 
 function updateGameArea() {
-    if (myGamePiece.crashWith(myObstacle)) {
-        myGameArea.stop();
+    var x, y;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
+        }
     }
-    else {
-        myGameArea.clear();
-        myObstacle.x += -2.5;
-        myGamePiece.newPos();
-        myGamePiece.update();
-        myObstacle.update();
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(20 + Math.floor(Math.random() * 101))) {
+        x = myGameArea.canvas.width;
+        y = myGameArea.canvas.height - 200
+        var oheight = Math.floor(Math.random() * 70) + 20;
+        myObstacles.push(new component(Math.floor(Math.random() * 13) + 2, oheight, "green", x, myGameArea.canvas.height - oheight));
     }
-}
-
-function moveup() {
-    if (myGamePiece.gravitySpeed == 0) {
-        myGamePiece.speedY -= 9;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -2.5;
+        myObstacles[i].update();
     }
+    myGamePiece.newPos();
+    myGamePiece.update();
 }
 
 
-function moveleft() {
-    myGamePiece.speedX -= 1;
-}
 
-function moveright() {
-    myGamePiece.speedX += 1;
-}
+kd.D.down(function() {
+    console.log('The "D" key is being held down!');
+    myGamePiece.width = 27;
+    myGamePiece.height = 13;
+    myGamePiece.image.src = "stickman.1.jpg";
+    myGamePiece.speedY = .2;
+    myGamePiece.speedX = 3;
+    myGamePiece.gravitySpeed = 0;
+});
+kd.A.down(function() {
+    console.log('The "A" key is being held down!');
+    myGamePiece.width = 27;
+    myGamePiece.height = 13;
+    myGamePiece.image.src = "stickman.2.jpg";
+    myGamePiece.speedY = .2;
+    myGamePiece.speedX = -3;
+    myGamePiece.gravitySpeed = 0;
+});
+
+kd.D.up(function() {
+    myGamePiece.width = 11;
+    myGamePiece.height = 21;
+    myGamePiece.image.src = "stickman.jpg";
+    myGamePiece.speedX = 0;
+});
+
+kd.A.up(function() {
+    myGamePiece.width = 11;
+    myGamePiece.height = 21;
+    myGamePiece.image.src = "stickman.jpg";
+    myGamePiece.speedX = 0;
+});
+
+kd.S.down(function() {
+    console.log('The "S" key is being held down!');
+    myGamePiece.speedY = 0;
+    myGamePiece.speedX = 0;
+    myGamePiece.gravitySpeed = 10;
+});
+
+kd.W.down(function() {
+    if (onBottom) {
+        myGamePiece.speedY -= 10;
+    }
+});
+
+// This update loop is the heartbeat of Keydrown
+kd.run(function() {
+    kd.tick();
+});
--- game/routes/index.js
+++ game/routes/index.js
@@ -9,6 +9,21 @@ router.get('/game.js', function (req,res,next)
     res.sendFile('game.js', {root:'public/javascripts'});
 })
 
+router.get('/stickman.jpg', function (req,res,next)
+{
+    res.sendFile('stickman.jpg', {root:'public/images'});
+})
+
+router.get('/stickman.1.jpg', function (req,res,next)
+{
+    res.sendFile('stickman.1.jpg', {root:'public/images'});
+})
+
+router.get('/stickman.2.jpg', function (req,res,next)
+{
+    res.sendFile('stickman.2.jpg', {root:'public/images'});
+})
+
 router.get('/', function (req,res,next)
 {
     res.sendFile('index.html', {root:'public'});
